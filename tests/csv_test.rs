46d5485b7232c9e2336db5619226ab11e4694a9d
Xqhare <xqhare@web.de>
2024-06-01 23:11:09 +0200

    tests
    created csv_test.rs

---
+++ tests/csv_test.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod csv_tests {
+    use json::*;
+    use pretty_assertions::{assert_eq, assert_ne, assert_str_eq};
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
