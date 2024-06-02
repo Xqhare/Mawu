25dd8a3f62059e383f2fe381e70249b4eb7a3e12
Xqhare <xqhare@web.de>
2024-06-02 17:41:36 +0200

    feat: CSV stub begun

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
 mod utils;
 pub mod errors;
+pub mod mawu_values;
 
+mod csv {
+    use std::path::Path;
+
+    use crate::{errors::MawuError, mawu_values::MawuValue};
+
+    pub fn from_csv_headed<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
+        todo!()
+    }
+
+    pub fn from_csv_headless<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
+        todo!()
+    }
+}
+
+
+pub fn testing() {
+    println!("Hello, world!");
+    let t = utils::file_handling::read_file("README.md").unwrap();
+    println!("{}", t);
+}
