85ab93ed7ae4099863732d02608915354745783a
Xqhare <xqhare@web.de>
2024-06-03 00:29:50 +0200

    feat: added CsvError::UnrecoginsedHeader

--- src/errors/csv_error.rs
+++ src/errors/csv_error.rs
@@ -3,6 +3,7 @@ use std::fmt;
 #[derive(Debug)]
 pub enum CsvError {
     ParseError(CsvParseError),
+    UnrecognizedHeader(String),
 }
 
 pub type Result<T> = std::result::Result<T, CsvError>;
@@ -11,6 +12,7 @@ impl fmt::Display for CsvError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             CsvError::ParseError(ref e) => e.fmt(f),
+            CsvError::UnrecognizedHeader(ref s) => write!(f, "Unrecognized header: {}", s),
         }
     }
     
@@ -21,9 +23,9 @@ pub enum CsvParseError {
     UnescapedDoubleQuote,
     UnterminatedQuote,
     UnescapedCharacter(char),
-    UnrecognizedHeader(String),
     ExtraValue(String),
     MissingValue(String),
+    UnexpectedNewline,
 }
 
 impl fmt::Display for CsvParseError {
@@ -32,9 +34,9 @@ impl fmt::Display for CsvParseError {
             CsvParseError::UnescapedDoubleQuote => write!(f, "Unescaped double quote"),
             CsvParseError::UnterminatedQuote => write!(f, "Unterminated quote"),
             CsvParseError::UnescapedCharacter(c) => write!(f, "Unescaped character: {}", c),
-            CsvParseError::UnrecognizedHeader(ref s) => write!(f, "Unrecognized header: {}", s),
             CsvParseError::ExtraValue(ref s) => write!(f, "Extra value: {}", s),
             CsvParseError::MissingValue(ref s) => write!(f, "Missing value: {}", s),
+            CsvParseError::UnexpectedNewline => write!(f, "Unexpected newline"),
         }
     }
 }
