f3041d1d486c870cde1156bd6843df17c54d577b
Xqhare <xqhare@web.de>
2024-06-03 00:31:02 +0200

    feat: MawuValues are now interactable

--- src/mawu_values/mod.rs
+++ src/mawu_values/mod.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum MawuValue {
+    CSVObject(Vec<HashMap<String, MawuValue>>),
     Object(HashMap<String, MawuValue>),
     Array(Vec<MawuValue>),
     Int(i32),
@@ -10,4 +12,124 @@ pub enum MawuValue {
     Bool(bool),
     Null,
 }
-    
+
+impl From<String> for MawuValue {
+    fn from(value: String) -> Self {
+        MawuValue::String(value)
+    }
+}
+
+impl MawuValue {
+    pub fn is_csv_object(&self) -> bool {
+        match self {
+            MawuValue::CSVObject(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_object(&self) -> bool {
+        match self {
+            MawuValue::Object(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_array(&self) -> bool {
+        match self {
+            MawuValue::Array(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_string(&self) -> bool {
+        match self {
+            MawuValue::String(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_int(&self) -> bool {
+        match self {
+            MawuValue::Int(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_float(&self) -> bool {
+        match self {
+            MawuValue::Float(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_bool(&self) -> bool {
+        match self {
+            MawuValue::Bool(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_null(&self) -> bool {
+        match self {
+            MawuValue::Null => true,
+            _ => false
+        }
+    }
+
+    pub fn as_csv_object(&self) -> Option<&Vec<HashMap<String, MawuValue>>> {
+        match self {
+            MawuValue::CSVObject(v) => Some(v),
+            _ => None
+        }
+    }
+
+    pub fn as_object(&self) -> Option<&HashMap<String, MawuValue>> {
+        match self {
+            MawuValue::Object(v) => Some(v),
+            _ => None
+        }
+    }
+
+    pub fn as_array(&self) -> Option<&Vec<MawuValue>> {
+        match self {
+            MawuValue::Array(v) => Some(v),
+            _ => None
+        }
+    }
+
+    pub fn as_string(&self) -> Option<&String> {
+        match self {
+            MawuValue::String(v) => Some(v),
+            _ => None
+        }
+    }
+
+    pub fn as_int(&self) -> Option<&i32> {
+        match self {
+            MawuValue::Int(v) => Some(v),
+            _ => None
+        }
+    }
+
+    pub fn as_float(&self) -> Option<&f32> {
+        match self {
+            MawuValue::Float(v) => Some(v),
+            _ => None
+        }
+    }
+
+    pub fn as_bool(&self) -> Option<&bool> {
+        match self {
+            MawuValue::Bool(v) => Some(v),
+            _ => None
+        }
+    }
+
+    pub fn as_null(&self) -> Option<&() > {
+        match self {
+            MawuValue::Null => Some(&()),
+            _ => None
+        }
+    }
+
+}
