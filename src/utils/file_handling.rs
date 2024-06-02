785b8e8d320a51633492bdf1e64c8133f32fb4f0
Xqhare <xqhare@web.de>
2024-06-02 17:42:02 +0200

    feat: file reader done

--- src/utils/file_handling.rs
+++ src/utils/file_handling.rs
@@ -1,5 +1,11 @@
-use std::path::Path;
+use std::{fs::read_to_string, path::Path};
 
-pub fn read_file<T: AsRef<Path>>(path: T) -> Result<String> {
-    
+use crate::errors::MawuError;
+
+/// This function reads the contents of a file and returns it as a single String.
+/// It only accepts valid UTF-8 encoded files, returning an error otherwise.
+pub fn read_file<T: AsRef<Path>>(path: T) -> Result<String, MawuError> {
+    let contents = read_to_string(path.as_ref())
+        .map_err(|e| MawuError::IoError(e))?;
+    Ok(contents)
 }
