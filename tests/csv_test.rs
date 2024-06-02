0212f73b8adc19e8fbe23c6e4c650bd7b8a146c4
Xqhare <xqhare@web.de>
2024-06-02 17:42:27 +0200

    feat: debug test started

--- tests/csv_test.rs
+++ tests/csv_test.rs
@@ -1,10 +1,17 @@
 #[cfg(test)]
 mod csv_tests {
     use json::*;
+    use mawu::testing;
     use pretty_assertions::{assert_eq, assert_ne, assert_str_eq};
 
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn tester() {
+        testing();
+        assert!(true);
     }
 }
