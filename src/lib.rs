bd0271699756b46720fd0d9194845cb28707ea49
Xqhare <xqhare@web.de>
2024-06-03 00:30:38 +0200

    feat: added in all csv methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,25 @@
 mod utils;
+mod lexers;
 pub mod errors;
 pub mod mawu_values;
 
-mod csv {
+pub mod csv {
     use std::path::Path;
 
-    use crate::{errors::MawuError, mawu_values::MawuValue};
+    use crate::{errors::MawuError, lexers::csv_lexer, mawu_values::MawuValue, utils::file_handling};
 
     pub fn from_csv_headed<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
-        todo!()
+        let file = file_handling::read_file(path)?;
+        csv_lexer::headed(file)
     }
 
     pub fn from_csv_headless<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
-        todo!()
+        let file = file_handling::read_file(path)?;
+        csv_lexer::headless(file)
+    }
+
+    pub fn from_csv_malformed<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
+        from_csv_headless(path)
     }
 }
 
-
-pub fn testing() {
-    println!("Hello, world!");
-    let t = utils::file_handling::read_file("README.md").unwrap();
-    println!("{}", t);
-}
