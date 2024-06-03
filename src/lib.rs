c8a81f104a2641bd41276df1bd63f90b6af71d0b
Xqhare <xqhare@web.de>
2024-06-03 23:47:32 +0200

    feat: simplified startup code

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,17 @@ pub mod errors;
 pub mod mawu_values;
 
 pub mod csv {
-    use std::path::Path;
+    use std::{collections::VecDeque, path::Path};
+    use unicode_segmentation::UnicodeSegmentation;
 
     use crate::{errors::MawuError, lexers::csv_lexer, mawu_values::MawuValue, utils::file_handling};
 
     pub fn from_csv_headed<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
-        let file = file_handling::read_file(path)?;
-        csv_lexer::headed(file)
+        csv_lexer::headed(file_handling::read_file(path)?.graphemes(true).collect::<VecDeque<&str>>())
     }
 
     pub fn from_csv_headless<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
-        let file = file_handling::read_file(path)?;
-        csv_lexer::headless(file)
+        csv_lexer::headless(file_handling::read_file(path)?.graphemes(true).collect::<VecDeque<&str>>())
     }
 
     pub fn from_csv_malformed<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
