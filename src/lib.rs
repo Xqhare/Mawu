c765b33237188a634f1943ac07315fe40278725f
Xqhare <xqhare@web.de>
2024-06-16 12:41:24 +0200

    cleanup: formatting and feat: renamed from_csv to read_csv

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,29 @@
-mod utils;
-mod lexers;
 pub mod errors;
+mod lexers;
 pub mod mawu_values;
+mod utils;
 
 pub mod csv {
     use std::{collections::VecDeque, path::Path};
     use unicode_segmentation::UnicodeSegmentation;
 
-    use crate::{errors::MawuError, lexers::csv_lexer, mawu_values::MawuValue, utils::file_handling};
+    use crate::{
+        errors::MawuError, lexers::csv_lexer, mawu_values::MawuValue, utils::file_handling,
+    };
 
-    pub fn from_csv_headed<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
-        csv_lexer::headed(file_handling::read_file(path)?.graphemes(true).collect::<VecDeque<&str>>())
+    pub fn read_csv_headed<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
+        csv_lexer::headed(
+            file_handling::read_file(path)?
+                .graphemes(true)
+                .collect::<VecDeque<&str>>(),
+        )
     }
 
-    pub fn from_csv_headless<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
-        csv_lexer::headless(file_handling::read_file(path)?.graphemes(true).collect::<VecDeque<&str>>())
+    pub fn read_csv_headless<T: AsRef<Path>>(path: T) -> Result<MawuValue, MawuError> {
+        csv_lexer::headless(
+            file_handling::read_file(path)?
+                .graphemes(true)
+                .collect::<VecDeque<&str>>(),
+        )
     }
-
 }
-
