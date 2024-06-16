8bff24f3cf4905479dd6238291cc6e6c54f6b485
Xqhare <xqhare@web.de>
2024-06-16 12:40:31 +0200

    cleanup: formatting

--- src/lexers/csv_lexer.rs
+++ src/lexers/csv_lexer.rs
@@ -1,7 +1,13 @@
 use std::collections::{HashMap, VecDeque};
 
-
-use crate::{errors::{csv_error::{CsvError, CsvParseError}, MawuError}, mawu_values::MawuValue, utils::is_newline};
+use crate::{
+    errors::{
+        csv_error::{CsvError, CsvParseError},
+        MawuError,
+    },
+    mawu_values::MawuValue,
+    utils::is_newline,
+};
 
 pub fn headed(file_contents: VecDeque<&str>) -> Result<MawuValue, MawuError> {
     let (head, left_content) = make_head(file_contents)?;
@@ -14,7 +20,9 @@ pub fn headed(file_contents: VecDeque<&str>) -> Result<MawuValue, MawuError> {
                 tmp_bind.insert(head[index].clone(), value.clone());
             }
         } else {
-            return Err(MawuError::CsvError(CsvError::ParseError(CsvParseError::ExtraValue(format!("{:?}", entry)))));
+            return Err(MawuError::CsvError(CsvError::ParseError(
+                CsvParseError::ExtraValue(format!("{:?}", entry)),
+            )));
         };
         out.push(tmp_bind);
     }
@@ -24,30 +32,49 @@ pub fn headed(file_contents: VecDeque<&str>) -> Result<MawuValue, MawuError> {
 pub fn headless(file_contents: VecDeque<&str>) -> Result<MawuValue, MawuError> {
     let (head, left_content) = make_head(file_contents)?;
     let mut body = parse_csv_body(left_content, head.len())?;
-    body.insert(0, head.into_iter().map(|s| MawuValue::from(s)).collect::<Vec<MawuValue>>());
+    body.insert(
+        0,
+        head.into_iter()
+            .map(|s| MawuValue::from(s))
+            .collect::<Vec<MawuValue>>(),
+    );
     Ok(MawuValue::CSVArray(body))
 }
 
 #[allow(unused_assignments)]
-fn parse_csv_body(mut csv_body: VecDeque<&str>, head_length: usize) -> Result<Vec<Vec<MawuValue>>, MawuError> {
+fn parse_csv_body(
+    mut csv_body: VecDeque<&str>,
+    head_length: usize,
+) -> Result<Vec<Vec<MawuValue>>, MawuError> {
     let mut out: Vec<Vec<MawuValue>> = Default::default();
-    let mut row_data: Vec<MawuValue> = Default::default();
+    let mut row_data: Vec<String> = Default::default();
     while csv_body.front().is_some() {
         if let Some(h) = csv_body.pop_front() {
+            let is_next_newline: bool = {
+                if let Some(k) = csv_body.front() {
+                    if is_newline(k) {
+                        true
+                    } else {
+                        false
+                    }
+                } else {
+                    // no new character => end of file!
+                    true
+                }
+            };
             if is_newline(h) {
-                if csv_body.is_empty() || is_newline(csv_body.front().ok_or_else(|| MawuError::CsvError(CsvError::ParseError(CsvParseError::UnexpectedNewline)))?) {
+                if is_next_newline
+                {
                     let _ = csv_body.pop_front();
-                }          
-                out.push(row_data);
+                }
+                out.push(row_data.iter().map(|s| MawuValue::from(s)).collect());
                 // assignment is only overwritten before being read if the very first character IS a newline and thus, probably, maybe, fine.
                 row_data = Default::default();
             } else if h == "," {
-                if is_newline(csv_body.front().ok_or_else(|| MawuError::CsvError(CsvError::ParseError(CsvParseError::UnexpectedNewline)))?) {
+                if is_next_newline && head_length > row_data.len() {
                     // push as many nulls as needed to fill in the missing data
-                    if head_length > row_data.len() {
-                        for _ in 0..(head_length - row_data.len()) {
-                            row_data.push(MawuValue::Null);
-                        }
+                    for _ in 0..(head_length - row_data.len()) {
+                        row_data.push(String::from(""));
                     }
                 }
             } else if h == "\"" {
@@ -67,13 +94,20 @@ fn parse_csv_body(mut csv_body: VecDeque<&str>, head_length: usize) -> Result<Ve
                         }
                     }
                 }
-                row_data.push(MawuValue::from(value));
+                row_data.push(value);
             } else {
                 let mut value: String = h.to_string();
-                while csv_body.front() != Some(&",") && !is_newline(csv_body.front().ok_or_else(|| MawuError::CsvError(CsvError::ParseError(CsvParseError::UnexpectedNewline)))?) {
+                while csv_body.front() != Some(&",") && !is_newline(csv_body.front().ok_or_else(|| {
+                        MawuError::CsvError(CsvError::ParseError(CsvParseError::UnexpectedNewline))
+                    })?)
+                {
                     if let Some(t) = csv_body.pop_front() {
                         let mut entry = t.to_string();
-                        while csv_body.front() != Some(&",") && !is_newline(csv_body.front().ok_or_else(|| MawuError::CsvError(CsvError::UnrecognizedHeader("".to_string())))?) {
+                        while csv_body.front() != Some(&",")
+                            && !is_newline(csv_body.front().ok_or_else(|| {
+                                MawuError::CsvError(CsvError::UnrecognizedHeader("".to_string()))
+                            })?)
+                        {
                             if let Some(g) = csv_body.pop_front() {
                                 entry.push_str(g);
                             }
@@ -81,28 +115,32 @@ fn parse_csv_body(mut csv_body: VecDeque<&str>, head_length: usize) -> Result<Ve
                         value.push_str(&entry);
                     }
                 }
-                row_data.push(MawuValue::from(value.clone()));
+                row_data.push(value);
             }
         }
     }
     Ok(out)
 }
 
-fn make_head(mut file_contents: VecDeque<&str>) -> Result<(Vec<String>, VecDeque<&str>), MawuError> {
+fn make_head(
+    mut file_contents: VecDeque<&str>,
+) -> Result<(Vec<String>, VecDeque<&str>), MawuError> {
     let mut head_done = false;
     let mut head_out: Vec<String> = Default::default();
     while !head_done {
-        if let Some(h) = file_contents.pop_front() {
-            if is_newline(h) {
+        if let Some(content) = file_contents.pop_front() {
+            if is_newline(content) {
                 head_done = true;
-            } else if h == "," {
+            } else if content == "," {
                 // do literally nothing
             } else {
-                if h == "\"" {
+                if content == "\"" {
                     let mut value: String = Default::default();
                     let mut open_quote = true;
                     while open_quote {
-                        if file_contents.front() == Some(&"\"") && file_contents.get(1) == Some(&"\"") {
+                        if file_contents.front() == Some(&"\"")
+                            && file_contents.get(1) == Some(&"\"")
+                        {
                             value.push_str("\"");
                             let _ = file_contents.pop_front();
                             let _ = file_contents.pop_front();
@@ -117,11 +155,21 @@ fn make_head(mut file_contents: VecDeque<&str>) -> Result<(Vec<String>, VecDeque
                     }
                     head_out.push(value);
                 } else {
-                    let mut value: String = h.to_string();
-                    while file_contents.front() != Some(&",") && !is_newline(file_contents.front().ok_or_else(|| MawuError::CsvError(CsvError::ParseError(CsvParseError::UnexpectedNewline)))?) {
+                    let mut value: String = content.to_string();
+                    while file_contents.front() != Some(&",") && !is_newline(file_contents.front().ok_or_else(|| {
+                            MawuError::CsvError(CsvError::ParseError(
+                                CsvParseError::UnexpectedNewline,
+                            ))
+                        })?)
+                    {
                         if let Some(t) = file_contents.pop_front() {
                             let mut entry = t.to_string();
-                            while file_contents.front() != Some(&",") && !is_newline(file_contents.front().ok_or_else(|| MawuError::CsvError(CsvError::UnrecognizedHeader("".to_string())))?) {
+                            while file_contents.front() != Some(&",") && !is_newline(file_contents.front().ok_or_else(|| {
+                                    MawuError::CsvError(CsvError::UnrecognizedHeader(
+                                        "".to_string(),
+                                    ))
+                                })?)
+                            {
                                 if let Some(g) = file_contents.pop_front() {
                                     entry.push_str(g);
                                 }
@@ -133,10 +181,12 @@ fn make_head(mut file_contents: VecDeque<&str>) -> Result<(Vec<String>, VecDeque
                 }
             }
         } else {
-            let t = file_contents.iter().map(|s| format!("{}", s)).collect::<String>();
+            let t = file_contents
+                .iter()
+                .map(|s| format!("{}", s))
+                .collect::<String>();
             return Err(MawuError::CsvError(CsvError::UnrecognizedHeader(t)));
         };
-    };
+    }
     Ok((head_out, file_contents))
 }
-
