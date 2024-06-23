f83c4baf0d4a0775f115a6b5a02f166b8e688f98
Xqhare <xqhare@web.de>
2024-06-23 23:36:03 +0200

    feat: added JsonError and created InternalError

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,11 +1,14 @@
 use std::{fmt, result};
 
 pub mod csv_error;
+pub mod json_error;
 
 #[derive(Debug)]
 pub enum MawuError {
     IoError(std::io::Error),
     CsvError(csv_error::CsvError),
+    JsonError(json_error::JsonError),
+    InternalError(MawuInternalError),
 }
 
 pub type Result<T> = result::Result<T, MawuError>;
@@ -15,6 +18,21 @@ impl fmt::Display for MawuError {
         match *self {
             MawuError::IoError(ref e) => e.fmt(f),
             MawuError::CsvError(ref e) => e.fmt(f),
+            MawuError::JsonError(ref e) => e.fmt(f),
+            MawuError::InternalError(ref e) => e.fmt(f),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum MawuInternalError {
+    UnableToLockMasterMutex,
+}
+
+impl fmt::Display for MawuInternalError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            MawuInternalError::UnableToLockMasterMutex => write!(f, "Unable to lock mutex"),
         }
     }
 }
