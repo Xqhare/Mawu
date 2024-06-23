b2a98b4b9e6746be2f6af60a93cc84ae54b5379c
Xqhare <xqhare@web.de>
2024-06-23 21:34:07 +0200

    feat: started work on json lexer

--- src/lexers/json_lexer.rs
+++ src/lexers/json_lexer.rs
@@ -1,1 +1,23 @@
+use std::collections::{HashMap, VecDeque};
 
+use crate::{
+    errors::{
+        csv_error::{CsvError, CsvParseError},
+        MawuError,
+    },
+    mawu_values::MawuValue,
+    utils::is_newline,
+};
+
+pub fn json_lexer(file_contents: &mut VecDeque<&str>) -> Result<MawuValue, MawuError> {
+
+    if file_contents.len() > 0 {
+        json_value_lexer(file_contents)
+    } else {
+        Ok(MawuValue::default())
+    }
+}
+
+fn json_value_lexer(file_contents: &mut VecDeque<&str>) -> Result<MawuValue, MawuError> {
+    
+}
