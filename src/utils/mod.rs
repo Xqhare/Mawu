cc4c20c732c4bea02e5686a35f836b39c85fae55
Xqhare <xqhare@web.de>
2024-06-24 21:17:25 +0200

    feat: added convenience functions is_end_primitive_value, is_digit and added comments
    is_digit is somewhat faster than the char standard library is_digit(10), but only slightly

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,14 @@
 use std::error::Error;
 
+use crate::errors::{MawuError, MawuInternalError};
+
 pub mod file_handling;
 
+/// Takes in a `&str` and checks if it is a newline character
+/// (any of `\n`, `\r\n`, `\r`)
+///
+/// ## Returns
+/// `true` if the string is a newline, `false` otherwise
 pub fn is_newline(s: &str) -> bool {
     s == "\n" || s == "\r\n" || s == "\r"
 }
@@ -31,12 +38,32 @@ pub fn unescape_unicode(s: &[u8]) -> Result<String, Box<dyn Error>> {
     Ok(String::from_utf8(output)?)
 }
 
-pub fn is_digit(c: &str) -> bool {
+/// Takes in a `&str` and checks the very first character to see if it is a digit
+///
+/// ## Returns
+/// `true` if the first character is a digit, `false` otherwise
+///
+/// ## Errors
+/// `MawuError::InternalError` if the string has no characters
+pub fn is_digit(c: &str) -> Result<bool, MawuError> {
     let charr = c.chars().next();
     if charr.is_some() {
-        charr.unwrap().is_digit(10)
+        // This if loop has proven to be faster than the char method `is_digit` by a very slight
+        // margin. But it is faster! (Using `match` is slower than both `if` and `char` methods)
+        if charr.unwrap() >= '0' && charr.unwrap() <= '9' {
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     } else {
-        false
+        Err(MawuError::InternalError(MawuInternalError::StringWithNoChars(c.to_string())))
     }
 }
 
+pub fn is_end_of_primitive_value(c: &str) -> bool {
+    c == "," || c == ":" || c == "}" || c == "]"
+}
+
+pub fn is_whitespace(c: &str) -> bool {
+    is_newline(c) || c == " " || c == "\t"
+}
