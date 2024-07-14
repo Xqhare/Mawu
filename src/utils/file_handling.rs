5156501dd2c65fdc423006954a6c10a53153f120
Xqhare <xqhare@web.de>
2024-07-14 13:00:40 +0200

    feat: read_file now constructs the deque of chars directly

--- src/utils/file_handling.rs
+++ src/utils/file_handling.rs
@@ -1,12 +1,12 @@
-use std::{fs::read_to_string, path::Path};
+use std::{collections::VecDeque, fs::read_to_string, path::Path};
 
 use crate::errors::MawuError;
 
-/// This function reads the contents of a file and returns it as a single String.
+/// This function reads the contents of a file, and converts the bytes from a Vec<u8> to a VecDeque<char>.
 /// It only accepts valid UTF-8 encoded files, returning an error otherwise.
-pub fn read_file<T: AsRef<Path>>(path: T) -> Result<String, MawuError> {
-    let contents = read_to_string(path.as_ref()).map_err(|e| MawuError::IoError(e))?;
-    Ok(contents)
+pub fn read_file<T: AsRef<Path>>(path: T) -> Result<VecDeque<char>, MawuError> {
+    let out = read_to_string(path.as_ref()).map_err(|e| MawuError::IoError(e))?.chars().collect::<VecDeque<char>>();
+    Ok(out)
 }
 
 /// This function writes a file with the given contents.
